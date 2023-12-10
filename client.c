a821f99b2ac6d4bd159aafd8779d6ce98292f2a6
b-filoni <138682845+b-filoni@users.noreply.github.com>
2023-12-10 09:37:39 -0300

    Update client.c

--- client.c
+++ client.c
@@ -3,40 +3,48 @@
 //#include <time.h>
 #include "bignumber.h"
 
-int main(int argc){
-    // clock_t start, end;
-    // double cpu_time_used;
-    // start = clock();
+int main() {
+    //clock_t start, end;
+    //double cpu_time_used;
+    //start = clock();
 
     VectorInt A, B, RES;
-    int operation;
     char operator;
 
-    A = vectorint();
-    B = vectorint();
-    RES = vectorint();
+    char fim;
+    while(fim != '.'){
+        A = vectorint();
+        B = vectorint();
+        RES = vectorint();
 
-    read_input(&A);
-    read_input(&B);
-    scanf("%c", &operator);
+        read_input(A);
+        read_input(B);
+        scanf("%c", &operator);
 
-    operation = choose_operation(operator, A->signal, B->signal);
+        switch (choose_operation(operator, A->signal, B->signal))
+        {
+        case 0:
+            subtracao(A, B, RES);
+            break;
+        case 1:
+            soma(A, B, RES);
+            break;
+        default:
+            multiplicacao(A, B, RES);
+            break;
+        }
 
-    if(operation == 0)
-        subtracao(&A, &B, &RES);
-    else if(operation == 1)
-        soma(&A, &B, &RES);
-    else
-        multiplicacao(&A, &B, &RES);
+        vectorint_print(RES);
+        //file_vectorint_print(RES, out);
 
-    vectorint_print(&RES);
+        vectorint_free(A);
+        vectorint_free(B);
+        vectorint_free(RES);
+        fim = getchar();
+    }   
 
-    vectorint_free(&A);
-    vectorint_free(&B);
-    vectorint_free(&RES);
-
-    // end = clock();
-    // cpu_time_used = ((double)(end - start)) / (CLOCKS_PER_SEC / 1000000);
-    // printf("\n\ntime: %.3fµs\n", cpu_time_used);
+    //end = clock();
+    //cpu_time_used = ((double)(end - start)) / (CLOCKS_PER_SEC / 1000);
+    //printf("\n\ntime: %.3fms\n", cpu_time_used);
     return 0;
 }
